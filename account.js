6bf47fe37b2dd8d781a837f152057ec444d70342
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-11-21 13:21:59 -0700

    Update and rename styles.css to account.js

---
+++ account.js
@@ -0,0 +1,6 @@
+// account.js
+const accounts = [
+    { username: 'jjsal1234', password: 'jacobsux1', verified: true },
+    { username: 'cow', password: 'cow', verified: true },
+    // Add more accounts as needed
+];
