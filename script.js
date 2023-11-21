9bfd537abcb4fc281c4ddc0e2ba10ea37d810c49
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-11-21 13:22:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    checkLoginStatus();
-});
-
+// script.js
 function login() {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    const usernameInput = document.getElementById('username').value;
+    const passwordInput = document.getElementById('password').value;
 
-    // For simplicity, use a hardcoded username and password
-    if (username === "user" && password === "pass") {
-        // Store the username in local storage
-        localStorage.setItem("loggedInUser", username);
-        checkLoginStatus();
+    const account = accounts.find(acc => acc.username === usernameInput && acc.password === passwordInput);
+
+    if (account) {
+        if (account.verified) {
+            alert('Login successful. Welcome!');
+        } else {
+            alert('Account not verified. Please contact support.');
+        }
     } else {
-        document.getElementById("error").innerText = "Invalid username or password";
-    }
-}
-
-function logout() {
-    // Remove the username from local storage
-    localStorage.removeItem("loggedInUser");
-    checkLoginStatus();
-}
-
-function checkLoginStatus() {
-    const loggedInUser = localStorage.getItem("loggedInUser");
-    const loginForm = document.getElementById("loginForm");
-    const welcomeMessage = document.getElementById("welcomeMessage");
-
-    if (loggedInUser) {
-        // User is logged in
-        loginForm.style.display = "none";
-        welcomeMessage.style.display = "block";
-        document.getElementById("loggedInUser").innerText = loggedInUser;
-    } else {
-        // User is not logged in
-        loginForm.style.display = "block";
-        welcomeMessage.style.display = "none";
-        document.getElementById("username").value = "";
-        document.getElementById("password").value = "";
-        document.getElementById("error").innerText = "";
+        alert('Invalid username or password. Please try again.');
     }
 }
