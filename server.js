d783c530816c008fa12905b84fb69e62c244b1b7
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-11-21 12:57:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,58 @@
+// server.js
 const express = require('express');
-const bodyParser = require('body-parser');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
-const port = 3000;
 
-app.use(bodyParser.json());
+// Passport setup
+passport.use(new GitHubStrategy({
+  clientID: 'YOUR_GITHUB_CLIENT_ID',
+  clientSecret: 'YOUR_GITHUB_CLIENT_SECRET',
+  callbackURL: 'http://localhost:3000/auth/github/callback',
+},
+function(accessToken, refreshToken, profile, done) {
+  // You can customize this function to save user data to your database
+  return done(null, profile);
+}));
 
-app.post('/signup', (req, res) => {
-    // Assuming you have a database to store user information
-    // In a real application, use a proper database library and hashing for passwords
+passport.use(new GoogleStrategy({
+  clientID: 'YOUR_GOOGLE_CLIENT_ID',
+  clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+},
+function(accessToken, refreshToken, profile, done) {
+  // You can customize this function to save user data to your database
+  return done(null, profile);
+}));
 
-    const userData = req.body;
-    console.log('Received sign-up request:', userData);
+app.use(passport.initialize());
+app.use(passport.session());
 
-    // Dummy response for demonstration purposes
-    res.json({ status: 'success', message: 'User signed up successfully' });
-});
+// Routes
+app.get('/auth/github',
+  passport.authenticate('github'));
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
