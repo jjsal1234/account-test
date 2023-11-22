a8685db8ff93bb20698868a2db5234e15e81c689
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-11-22 01:32:15 -0700

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,24 +1,40 @@
-// Define user accounts
-var accounts = [
-    { username: "jjsal1234", password: "jacobsux1", verified: true, banned: false },
-    { username: "Skeleton", password: "DfcKyt", verified: true, banned: false },
-    { username: "User", password: "AgsJnsHHGJSjjsdi", verified: false, banned: true },
-    // Add more accounts with the banned attribute
+// accounts.js
+
+// Sample accounts data with the "Locked" attribute
+const accounts = [
+    { username: "Tester", password: "password2", verified: true, banned: false, locked: true },
+    { username: "jjsal1234", password: "jacobsux1omg", verified: true, banned: false, locked: false },
+    { username: "Skeleton", password: "DfcKyt", verified: true, banned: false, locked: false },
+    { username: "Cow", password: "HsdjaHjdJ", verified: false, banned: false, locked: false },
+    // Add more accounts as needed
 ];
 
-// Function to find an account by username
-function findAccount(username) {
-    return accounts.find(account => account.username === username);
+// Function to check if an account is locked
+function isAccountLocked(username) {
+    const account = getAccountInfo(username);
+    return account && account.locked;
 }
 
-// Function to check if the provided username and password match an account
-function checkCredentials(username, password) {
-    const account = findAccount(username);
-    return account && account.password === password;
-}
+// Function to sign in
+function signIn() {
+    const username = document.getElementById("username").value;
+    const password = prompt("Please enter your password"); // Simple password prompt (replace with a more secure method)
 
-// Function to get account information
-function getAccountInfo(username) {
-    const account = findAccount(username);
-    return account ? { username: account.username, verified: account.verified } : null;
+    const accountInfo = getAccountInfo(username);
+
+    if (accountInfo && !accountInfo.banned && !isAccountLocked(username) && checkCredentials(username, password)) {
+        alert("Sign-in successful!");
+        closeSignInPopup();
+        openProfilePopup(username, accountInfo.verified);
+        // Set a cookie or use localStorage to persist the sign-in state
+        localStorage.setItem("signedInUser", username);
+        updateSignInStatus();
+        updateLeaderboard();
+    } else if (accountInfo && accountInfo.banned) {
+        window.location.href = "https://jjsal1234.github.io/Coolsite/banned";
+    } else if (isAccountLocked(username)) {
+        alert("This account is locked.");
+    } else {
+        alert("Incorrect username or password. Please try again.");
+    }
 }
