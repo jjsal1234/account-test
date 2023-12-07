28fa4ef2c87c3456291eeb90133f6ca5f997d959
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-12-07 08:25:42 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,27 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-  ui.start("#firebaseui-auth-container", {
-    signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      // Add more sign-in providers as needed
-    ],
-    signInSuccessUrl: "/",
-    signInFlow: "popup",
-  });
+    ui.start("#firebaseui-auth-container", {
+        signInOptions: [
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID
+        ],
+        signInSuccessUrl: "/",
+        signInFlow: "popup",
+    });
 
-  document.getElementById("sign-out").addEventListener("click", function () {
-    firebase.auth().signOut();
-  });
+    document.getElementById("sign-out").addEventListener("click", function () {
+        firebase.auth().signOut();
+    });
 
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-      // User is signed in
-      console.log("Signed in:", user.displayName);
-    } else {
-      // User is signed out
-      console.log("Signed out");
-    }
-  });
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in
+            console.log("Signed in:", user.displayName);
+        } else {
+            // User is signed out
+            console.log("Signed out");
+        }
+    });
 });
