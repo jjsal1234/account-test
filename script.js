36de39162ec2641560103c0a56555fce812c2f4e
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-12-07 15:59:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,67 @@
-function encryptData(data) {
-    const key = 'DEVELOPMENT_ENCRYPTION_KEY'; // Change this to a secure secret key
-    return CryptoJS.AES.encrypt(JSON.stringify(data), key).toString();
+document.getElementById('hackButton').addEventListener('click', function () {
+    hack();
+});
+
+function hack() {
+    const consoleElement = document.querySelector('.console');
+    consoleElement.innerHTML = '';
+
+    // Simulate password cracking
+    crackPassword()
+        .then(() => {
+            writeToConsole('Connecting to the server...');
+            setTimeout(() => {
+                writeToConsole('Gaining access to the database...');
+                setTimeout(() => {
+                    writeToConsole('Extracting sensitive information...');
+                    setTimeout(() => {
+                        writeToConsole('Hacking successful! Data acquired.');
+                    }, 2000);
+                }, 2000);
+            }, 2000);
+        })
+        .catch(() => {
+            writeToConsole('Access denied. Incorrect password.');
+        });
 }
 
-function decryptData(encryptedData) {
-    const key = 'DEVELOPMENT_ENCRYPTION_KEY'; // Change this to the same secret key used for encryption
-    const bytes = CryptoJS.AES.decrypt(encryptedData, key);
-    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+function crackPassword() {
+    return new Promise((resolve, reject) => {
+        const targetPassword = generateRandomPassword();
+        const attempts = 3;
+
+        // Prompt user for password input
+        const enteredPassword = prompt('Enter the password:');
+
+        // Simulate password checking
+        setTimeout(() => {
+            if (enteredPassword === targetPassword) {
+                writeToConsole('Password accepted. Proceeding to the next step...');
+                resolve();
+            } else {
+                writeToConsole(`Incorrect password. ${attempts} attempts remaining...`);
+                if (attempts > 1) {
+                    crackPassword().then(resolve).catch(reject);
+                } else {
+                    reject();
+                }
+            }
+        }, 1000);
+    });
 }
 
-function readUserData() {
-    const encryptedData = localStorage.getItem('user_data');
-    return encryptedData ? decryptData(encryptedData) : [];
+function generateRandomPassword() {
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+    let password = '';
+    for (let i = 0; i < 8; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters.charAt(randomIndex);
+    }
+    return password;
 }
 
-function writeUserData(data) {
-    const encryptedData = encryptData(data);
-    localStorage.setItem('user_data', encryptedData);
+function writeToConsole(message) {
+    const consoleElement = document.querySelector('.console');
+    consoleElement.innerHTML += `<p>${message}</p>`;
+    consoleElement.scrollTop = consoleElement.scrollHeight;
 }
-
-// Example usage
-const userData = readUserData();
-console.log('Read user data:', userData);
-
-// Add new user
-const newUser = { username: 'newuser', password: 'newpassword' };
-userData.push(newUser);
-writeUserData(userData);
-console.log('User added:', newUser);
