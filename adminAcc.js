4596e418ad4bb350de6dd88815470541f8536a3a
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-12-12 23:50:32 -0700

    Update and rename script.js to adminAcc.js

---
+++ adminAcc.js
@@ -0,0 +1,5 @@
+// Simulated account data with hashed passwords
+const accounts = [
+    { username: 'admin', passwordHash: '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8' }, // password: admin123
+    // Add more accounts as needed
+];
