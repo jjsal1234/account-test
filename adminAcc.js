d7d071cd5abb91d3e5ca8e66e670ee66af73134d
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-12-13 12:22:17 -0700

    Update adminAcc.js

--- adminAcc.js
+++ adminAcc.js
@@ -1,5 +1,5 @@
-// Simulated account data with corrected password hash
+// Simulated account data with plain text passwords
 const accounts = [
-    { username: 'admin', passwordHash: '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8' }, // password: admin123
+    { username: 'admin', password: 'admin123' },
     // Add more accounts as needed
 ];
