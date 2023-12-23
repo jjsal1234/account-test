cede6fa2a857a450d1d1ec7d8f9b71f7c6fd9c33
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-12-23 01:40:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function register() {
     const xhr = new XMLHttpRequest();
 
     // Specify the type of request and the URL
-    xhr.open('POST', 'https://your-000webhost-url/accounts.php', true);
+    xhr.open('POST', 'https://c00lsite.000webhostapp.com/accounts.php', true);
 
     // Set the request header
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
@@ -34,6 +34,41 @@ function register() {
 }
 
 function login() {
-    // Similar logic for login as in the previous example
-    // Implement this part based on your needs
+    // Get user input
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+
+    // Create an XMLHttpRequest object
+    const xhr = new XMLHttpRequest();
+
+    // Specify the type of request and the URL
+    xhr.open('POST', 'https://c00lsite.000webhostapp.com/accounts.php', true);
+
+    // Set the request header
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    // Set up a function to handle the response
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                // Check if the login was successful
+                if (xhr.responseText === 'Login successful') {
+                    // Redirect or perform any action for successful login
+                    console.log('Login successful');
+                } else {
+                    // Login failed
+                    console.error('Login failed');
+                }
+            } else {
+                // Request failed
+                console.error('Error during login request');
+            }
+        }
+    };
+
+    // Prepare the request parameters
+    const params = `action=login&username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
+
+    // Send the request
+    xhr.send(params);
 }
